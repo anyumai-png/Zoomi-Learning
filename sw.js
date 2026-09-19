4d0f897a3287a722ed632a28f8b69e288a62f3e3
anyumai-png <andyyum.ai@gmail.com>
2026-09-20 00:47:25 +0800

    Add service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='hanzi-adventure-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}).catch(()=>caches.match('./index.html'))))});
